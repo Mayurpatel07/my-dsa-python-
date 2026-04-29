29ef26473c529822f6cad5c9e7433d61928f4b43
Mayur Patel <patel.mayur2113@gmail.com>
2026-04-29 13:33:00 +0530

    feat(python): add loops notebook (for, while)

--- 1.Python/modules/package/math.py
+++ 1.Python/modules/package/math.py
@@ -2,3 +2,6 @@ def addition(a,b):
     return a+b
 # Update: 2026-04-29 10:35:00 - feat(python): add magic methods examples
 
+
+# Update: 2026-04-29 13:33:00 - feat(python): add loops notebook (for, while)
+
